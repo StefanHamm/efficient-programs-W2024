3e9e8402a249c93f533fe3914899d0e330113f83
thuber19 <52424519+thuber19@users.noreply.github.com>
2025-01-02 18:16:33 +0100

    remov v3

--- src/v3/joiner.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef JOINER_H
-#define JOINER_H
-
-#include <string>
-//definiton of the join function in the header file
-int join(std::string path1, std::string path2, std::string path3, std::string path4);
-
-#endif
