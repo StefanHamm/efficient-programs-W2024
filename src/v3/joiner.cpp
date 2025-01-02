5160fcd4098835b5a71fed4d2c2139dcd4c94de4
thuber19 <52424519+thuber19@users.noreply.github.com>
2025-01-02 19:31:09 +0100

    revert v3

--- src/v3/joiner.cpp
+++ src/v3/joiner.cpp
@@ -10,15 +10,23 @@ struct Table1Entry {
     std::vector<std::string> Cs;
 };
 
+void parseLine(const std::string& line, char delimiter, std::string& part1, std::string& part2) {
+    size_t delimPos = line.find(delimiter);
+    if (delimPos != std::string::npos) {
+        part1 = line.substr(0, delimPos);
+        part2 = line.substr(delimPos + 1);
+    }
+}
+
 int hashJoin(const std::string& path1, const std::string& path2, const std::string& path3, const std::string& path4) {
     std::unordered_map<std::string, Table1Entry> table1; // Key: A, Values: Bs and Cs
     table1.reserve(20000000);
 
     std::unordered_map<std::string, std::vector<std::string>> table3; // Key: A, Values: D's
-    table3.reserve(15000000);
+    table3.reserve(20000000);
 
     std::unordered_map<std::string, std::vector<std::string>> table4; // Key: D, Values: E's
-    table4.reserve(15000000);
+    table4.reserve(20000000);
 
 
     
@@ -30,36 +38,21 @@ int hashJoin(const std::string& path1, const std::string& path2, const std::stri
     }
     std::string line, A, B;
     while (std::getline(file1, line)) {
-        size_t delimPos = line.find(',');
-        if (delimPos != std::string::npos) {
-            A = line.substr(0, delimPos);
-            B = line.substr(delimPos + 1);
-            //if (table1[A].Bs.empty()) {
-            //    table1[A].Bs.reserve(1000000);
-            //}
-            table1[A].Bs.push_back(B);
-        }
-        
+        parseLine(line, ',', A, B);
+        table1[A].Bs.push_back(B);
     }
 
     // Read File2 (A,C)
     std::ifstream file2(path2);
+    std::string C;
     if (!file2.is_open()) {
         std::cerr << "Error opening File2\n";
         return -1;
     }
     std::string C;
     while (std::getline(file2, line)) {
-        size_t delimPos = line.find(',');
-        if (delimPos != std::string::npos) {
-            A = line.substr(0, delimPos);
-            C = line.substr(delimPos + 1);
-            //if (table1[A].Cs.empty()) {
-            //    table1[A].Cs.reserve(1000000); // Initial reserve for small Cs
-            //}
-            table1[A].Cs.push_back(C);
-
-        }
+        parseLine(line, ',', A, C);
+        table1[A].Cs.push_back(C);
     }
 
     // Read File3 (A,D)
@@ -70,15 +63,8 @@ int hashJoin(const std::string& path1, const std::string& path2, const std::stri
     }
     std::string D;
     while (std::getline(file3, line)) {
-        size_t delimPos = line.find(',');
-        if (delimPos != std::string::npos) {
-            A = line.substr(0, delimPos);
-            D = line.substr(delimPos + 1);
-            //if (table3[A].empty()) {
-            //    table3[A].reserve(1000000); // Initial reserve for small Cs
-            //}
-            table3[A].push_back(D);
-        }
+        parseLine(line, ',', A, D);
+        table3[A].push_back(D);
     }
 
     // Read File4 (D,E)
@@ -89,16 +75,8 @@ int hashJoin(const std::string& path1, const std::string& path2, const std::stri
     }
     std::string E;
     while (std::getline(file4, line)) {
-        size_t delimPos = line.find(',');
-        if (delimPos != std::string::npos) {
-            D = line.substr(0, delimPos);
-            E = line.substr(delimPos + 1);
-
-            //if (table4[D].empty()) {
-            //    table4[D].reserve(1000000); // Initial reserve for small Es
-            //}
-            table4[D].push_back(E);
-        }
+        parseLine(line, ',', D, E);
+        table4[D].push_back(E);
     }
 
     // Perform the join
