0e8a1e37d6bb9be22f2603600a8d6bd92d54ac95
Vinz <vinzent.beer@gmail.com>
2025-01-18 20:02:47 +0100

    try mmap fix

--- src/v8/joiner.cpp
+++ src/v8/joiner.cpp
@@ -4,206 +4,137 @@
 #include <unordered_map>
 #include <vector>
 #include <string>
-#include <algorithm>
-#include <immintrin.h>
 #include <sys/mman.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
 #include <fcntl.h>
-#include "joiner.h"
-
-// Chunk size (adjust based on your system's memory and performance characteristics)
-const size_t CHUNK_SIZE = 1024 * 1024 * 100; // 100 MB
-const int VECTOR_SIZE = 16; // SIMD vector size
+#include <unistd.h>
+#include <cstring>
+#include <algorithm>
+#include <x86intrin.h>
 
 struct Table1Entry {
     std::vector<std::string> Bs;
     std::vector<std::string> Cs;
 };
 
-// Optimized parsing function using SIMD to find delimiters and split lines
-void parseLine(const char* line, size_t lineSize, char delimiter, std::string& part1, std::string& part2) {
-     size_t delimPos = std::string::npos;
-    __m128i delimiterVec = _mm_set1_epi8(delimiter);
-    size_t i = 0;
-    for (; i + VECTOR_SIZE <= lineSize; i += VECTOR_SIZE) {
-        __m128i chunk = _mm_loadu_si128(reinterpret_cast<const __m128i*>(line + i));
-        __m128i mask = _mm_cmpeq_epi8(chunk, delimiterVec);
-        unsigned int maskValue = _mm_movemask_epi8(mask);
-        if (maskValue != 0) {
-            delimPos = i + __builtin_ctz(maskValue);
-            break;
-        }
+
+// Helper function to get file size
+size_t getFileSize(int fd) {
+    struct stat st;
+    if (fstat(fd, &st) == -1) {
+        return 0;
     }
-    if (delimPos == std::string::npos) {
-        for (; i < lineSize; ++i) {
-            if (line[i] == delimiter) {
-                delimPos = i;
-                break;
+    return st.st_size;
+}
+
+// Helper function to map file into memory
+char* mapFile(const std::string& path, size_t& fileSize) {
+    int fd = open(path.c_str(), O_RDONLY);
+    if (fd == -1) {
+        return nullptr;
+    }
+    fileSize = getFileSize(fd);
+    if (fileSize == 0) {
+        close(fd);
+        return nullptr;
+    }
+    char* addr = (char*)mmap(nullptr, fileSize, PROT_READ, MAP_PRIVATE, fd, 0);
+    close(fd); // File descriptor is no longer needed after mmap
+    if (addr == MAP_FAILED) {
+        return nullptr;
+    }
+    return addr;
+}
+
+void unmapFile(char* addr, size_t fileSize) {
+    if (addr != nullptr) {
+        munmap(addr, fileSize);
+    }
+}
+
+
+// New parseLine function that operates on memory mapped data
+void parseLineMMAP(const char* data, size_t& pos, size_t fileSize, char delimiter, std::string& part1, std::string& part2) {
+    size_t start = pos;
+    while (pos < fileSize && data[pos] != delimiter && data[pos] != '\n') {
+        pos++;
+    }
+    if (pos < fileSize) {
+        part1.assign(data + start, pos - start);
+        if (data[pos] == delimiter)
+        {
+            start = ++pos;
+            while (pos < fileSize && data[pos] != '\n') {
+                pos++;
             }
+            part2.assign(data + start, pos - start);
         }
-    }
-    if (delimPos != std::string::npos) {
-        part1 = std::string(line, 0, delimPos);
-        part2 = std::string(line, delimPos + 1, lineSize - (delimPos+1));
-    }
-}
-
-
-// Function to process a chunk of a memory mapped file (generic)
-template <typename TableType>
-void processChunk(const char* fileStart, size_t fileSize, char delimiter, TableType& table) {
-    const char* current = fileStart;
-    const char* end = fileStart + fileSize;
-     while (current < end) {
-          const char* lineStart = current;
         
-          while (current < end && *current != '\n') {
-             ++current;
-          }
-          size_t lineSize = current - lineStart;
-        
-           if (lineSize > 0) {
-                std::string line, key, value;
-                parseLine(lineStart, lineSize, delimiter, key, value);
-                table[key].push_back(value);
-            }
-          ++current;
-     }
-}
-
-// Function to process a chunk of file1 and file2 (specific)
-void processChunk(const char* file1Start, size_t file1Size, const char* file2Start, size_t file2Size, char delimiter, std::unordered_map<std::string, Table1Entry>& table1, bool isFirstChunk) {
-    const char* current1 = file1Start;
-    const char* end1 = file1Start + file1Size;
-
-     const char* current2 = file2Start;
-     const char* end2 = file2Start + file2Size;
-
-
-    if (isFirstChunk) {
-         while(current1 < end1) {
-             const char* lineStart = current1;
-             while(current1 < end1 && *current1 != '\n') ++current1;
-              size_t lineSize = current1 - lineStart;
-
-              if (lineSize > 0) {
-                   std::string line, A, B;
-                  parseLine(lineStart, lineSize, delimiter, A, B);
-                  table1[A].Bs.push_back(B);
-              }
-            ++current1;
-        }
-
-      while(current2 < end2) {
-           const char* lineStart = current2;
-             while(current2 < end2 && *current2 != '\n') ++current2;
-             size_t lineSize = current2 - lineStart;
-
-           if(lineSize > 0) {
-             std::string line, A, C;
-              parseLine(lineStart, lineSize, delimiter, A, C);
-              table1[A].Cs.push_back(C);
-         }
-            ++current2;
-      }
-    } else {
-         // Re-process to update table1. Note that memory mapped files dont need to be "reset", and we simply iterate from the beginning of the file
-       
-          while(current1 < end1) {
-             const char* lineStart = current1;
-             while(current1 < end1 && *current1 != '\n') ++current1;
-             size_t lineSize = current1 - lineStart;
-
-           if (lineSize > 0) {
-                  std::string line, A, B;
-                   parseLine(lineStart, lineSize, delimiter, A, B);
-                    table1[A].Bs.push_back(B);
-              }
-              ++current1;
-        }
-
-      while(current2 < end2) {
-           const char* lineStart = current2;
-             while(current2 < end2 && *current2 != '\n') ++current2;
-            size_t lineSize = current2 - lineStart;
-
-          if(lineSize > 0) {
-               std::string line, A, C;
-                 parseLine(lineStart, lineSize, delimiter, A, C);
-                  table1[A].Cs.push_back(C);
-             }
-            ++current2;
-        }
+        if (pos < fileSize && data[pos] == '\n') pos++;
     }
 }
-
 
 int hashJoin(const std::string& path1, const std::string& path2, const std::string& path3, const std::string& path4) {
     std::unordered_map<std::string, Table1Entry> table1;
+    table1.reserve(20000000);
+
     std::unordered_map<std::string, std::vector<std::string>> table3;
+    table3.reserve(20000000);
+
     std::unordered_map<std::string, std::vector<std::string>> table4;
+    table4.reserve(20000000);
 
-    // Open all files
-    int fd1 = open(path1.c_str(), O_RDONLY);
-    int fd2 = open(path2.c_str(), O_RDONLY);
-    int fd3 = open(path3.c_str(), O_RDONLY);
-    int fd4 = open(path4.c_str(), O_RDONLY);
-    if (fd1 == -1 || fd2 == -1 || fd3 == -1 || fd4 == -1) {
-      std::cerr << "Error opening files\n";
+    // Memory-map the files
+    size_t fileSize1, fileSize2, fileSize3, fileSize4;
+    char* file1_data = mapFile(path1, fileSize1);
+    char* file2_data = mapFile(path2, fileSize2);
+    char* file3_data = mapFile(path3, fileSize3);
+    char* file4_data = mapFile(path4, fileSize4);
+
+    if (!file1_data || !file2_data || !file3_data || !file4_data) {
+        std::cerr << "Error mapping files\n";
+        unmapFile(file1_data, fileSize1);
+        unmapFile(file2_data, fileSize2);
+        unmapFile(file3_data, fileSize3);
+        unmapFile(file4_data, fileSize4);
         return -1;
     }
 
-    // Get file sizes
-    struct stat st1, st2, st3, st4;
-    fstat(fd1, &st1);
-    fstat(fd2, &st2);
-    fstat(fd3, &st3);
-    fstat(fd4, &st4);
-    size_t size1 = st1.st_size;
-    size_t size2 = st2.st_size;
-    size_t size3 = st3.st_size;
-    size_t size4 = st4.st_size;
-
-
-    // Map the files into memory
-    const char* file1_mmap = static_cast<const char*>(mmap(nullptr, size1, PROT_READ, MAP_PRIVATE, fd1, 0));
-    const char* file2_mmap = static_cast<const char*>(mmap(nullptr, size2, PROT_READ, MAP_PRIVATE, fd2, 0));
-    const char* file3_mmap = static_cast<const char*>(mmap(nullptr, size3, PROT_READ, MAP_PRIVATE, fd3, 0));
-    const char* file4_mmap = static_cast<const char*>(mmap(nullptr, size4, PROT_READ, MAP_PRIVATE, fd4, 0));
-     if (file1_mmap == MAP_FAILED || file2_mmap == MAP_FAILED || file3_mmap == MAP_FAILED || file4_mmap == MAP_FAILED) {
-        std::cerr << "Error memory mapping files\n";
-         close(fd1);
-         close(fd2);
-         close(fd3);
-         close(fd4);
-        return -1;
+    // Read File1 (A,B)
+    size_t pos = 0;
+    std::string A, B;
+    while (pos < fileSize1) {
+       parseLineMMAP(file1_data, pos, fileSize1, ',', A, B);
+       if (!A.empty()) table1[A].Bs.push_back(B);
     }
 
-    // Process file1 and file2
-    bool isFirstChunk = true;
-     processChunk(file1_mmap, size1, file2_mmap, size2, ',', table1, isFirstChunk);
+
+    // Read File2 (A,C)
+    pos = 0;
+    std::string C;
+    while (pos < fileSize2) {
+       parseLineMMAP(file2_data, pos, fileSize2, ',', A, C);
+       if (!A.empty()) table1[A].Cs.push_back(C);
+    }
 
 
-    // Process file3
-    processChunk(file3_mmap, size3, ',', table3);
-
-    // Process file4
-     processChunk(file4_mmap, size4, ',', table4);
-
-    // Unmap the files
-    munmap( (void *)file1_mmap, size1);
-    munmap( (void *)file2_mmap, size2);
-    munmap( (void *)file3_mmap, size3);
-    munmap( (void *)file4_mmap, size4);
+    // Read File3 (A,D)
+    pos = 0;
+    std::string D;
+    while (pos < fileSize3) {
+       parseLineMMAP(file3_data, pos, fileSize3, ',', A, D);
+       if (!A.empty()) table3[A].push_back(D);
+    }
 
 
-    close(fd1);
-    close(fd2);
-    close(fd3);
-    close(fd4);
-
+    // Read File4 (D,E)
+    pos = 0;
+    std::string E;
+    while (pos < fileSize4) {
+       parseLineMMAP(file4_data, pos, fileSize4, ',', D, E);
+       if (!D.empty()) table4[D].push_back(E);
+    }
 
     // Perform the join
     for (const auto& [A, entry] : table1) {
@@ -222,9 +153,13 @@ int hashJoin(const std::string& path1, const std::string& path2, const std::stri
         }
     }
 
+    // Unmap memory
+    unmapFile(file1_data, fileSize1);
+    unmapFile(file2_data, fileSize2);
+    unmapFile(file3_data, fileSize3);
+    unmapFile(file4_data, fileSize4);
     return 0;
 }
-
 
 int join(const std::string path1, const std::string path2, const std::string path3, const std::string path4) {
     int code = hashJoin(path1, path2, path3, path4);
